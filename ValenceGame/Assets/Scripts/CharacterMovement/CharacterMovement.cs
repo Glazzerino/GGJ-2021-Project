8e66660ff23f804fcd0ceb7db48588dc1319ed16
Facundo <facundo.vecchi@outlook.com>
2021-01-30 17:25:04 -0600

    Test

---
+++ ValenceGame/Assets/Scripts/CharacterMovement/CharacterMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public CharacterController2D controller;
+    public Transform shape;
+
+    float horizontalMove = 0f;
+    float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update(){
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch")) {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
---
+++ ValenceGame/Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Grid
+{
+    private int height;
+    private int width;
+    private float cellSize;
+
+    private int[,] GridArray;
+    private TextMesh[,] debugTextArray;
+
+    public Grid(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        GridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
+
+        for (int x=0; x<GridArray.GetLength(0); x++)
+        {
+            for(int y=0; y <GridArray.GetLength(1); y++)
+            {
+                debugTextArray[x,y] = UtilsClass.CreateWorldText(GridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 20, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x+1, y), Color.white, 100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+        setValue(2, 1, 56);
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize;
+    }
+
+
+    public void setValue(int x, int y, int value)
+    {
+        if(x >= 0 && y >= 0 && x < width && y < height)
+        {
+
+        }
+        GridArray[x, y] = value;
+        debugTextArray[x, y].text = GridArray[x, y].ToString();
+    }
+}
