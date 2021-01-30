f90a26ac8b4b037dc18bfe57cada39879f52ebb8
fztmo <fztmon@gmail.com>
2021-01-30 17:50:10 -0600

    added controller

---
+++ Valence/Assets/Scripts/GrappableEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappableEntity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static readonly HashSet<GrappableEntity> Entities = new HashSet<GrappableEntity>();
+    void Start()
+    {
+        Entities.Add(this);
+    }
+
+    private void OnDestroy()
+    {
+        Entities.Remove(this);
+    }
+}
