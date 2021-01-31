384f34f0f2777a1a3c10545d0ff6d7bbdeb0d233
LuisTamez <69485857+LuisTamez@users.noreply.github.com>
2021-01-31 16:06:01 -0600

    Added startbutton in main menu

---
+++ Valence/Assets/Scripts/startGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    public void gameBegin()
+    {
+        SceneManager.LoadScene("FirstLevel");
+    }
+}
