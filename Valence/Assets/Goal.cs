4fe6b2891867d91998f84ddcea1c864e0cef8a8f
fztmo <fztmon@gmail.com>
2021-01-31 17:40:57 -0600

    scene change

---
+++ Valence/Assets/Goal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    
+    Scene nextLevel;
+    void Start()
+    {
+    }
+    private void OnCollisionEnter2D( Collision2D collision ) {
+           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
