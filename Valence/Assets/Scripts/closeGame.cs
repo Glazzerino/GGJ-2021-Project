8acacb172308cba2632572528c4846aa24b855f5
LuisTamez <69485857+LuisTamez@users.noreply.github.com>
2021-01-31 16:37:24 -0600

    Completed Menu and set up aditional components

---
+++ Valence/Assets/Scripts/closeGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closeGame : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
